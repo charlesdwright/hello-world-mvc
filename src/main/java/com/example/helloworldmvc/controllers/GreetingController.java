afbdb6d56406615481bdf67ad7ccc736baa3b4ee
charlesdwright <71340308+charlesdwright@users.noreply.github.com>
2021-05-04 15:30:14 -0400

    intial commit

---
+++ src/main/java/com/example/helloworldmvc/controllers/GreetingController.java
@@ -0,0 +1,16 @@
+package com.example.helloworldmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="variable",required=false,
+            defaultValue = "Yo Mammy") String variable, Model model) {
+        model.addAttribute("variable", variable);
+        return "greeting";
+    }
+}
